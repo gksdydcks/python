150afa14440c443db1f6cd1ec478e23034ddde24
python <dydcks1001@gmail.com>
2024-11-29 13:50:09 +0900

    print2

--- test/1.py
+++ test/1.py
@@ -2,4 +2,13 @@ print("hello", "python", 1, 2, sep = "_" )
 print()
 print("1")
 print("안녕하세요", end = "__")
-print("한용찬입니다.")
+print("코딩온입니다.")
+ive = "I AM"
+print(ive)
+ive = "장원영"
+print (ive)
+Ive = "카리나"
+print (Ive)
+# my_home : snake_case
+# Myhome : CamelCase
+# myHome : PascalCase
